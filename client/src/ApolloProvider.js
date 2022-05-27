571ca2ea7e5a5643425b3cfa753b54619c34b7a5
divyanshpandey03396 <divyanshpandey03396@gmail.com>
2022-05-27 23:13:36 +0530

    heroku link added

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -8,7 +8,7 @@ import {
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://socio-app-backend.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
